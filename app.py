bb22a0f61c9fcaa5c4a82194dd5350f39de72ddd
KushalKUMAR-P <kushal44sed@gmail.com>
2026-02-19 23:22:55 -0600

    Fix production session issue with fallback secret key and proxy configuration

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from models.user_model import create_user, get_user_by_email
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
+print("SECRET KEY:", app.secret_key)
 
 
 # ✅ Initialize DB safely on startup (works for Gunicorn)
